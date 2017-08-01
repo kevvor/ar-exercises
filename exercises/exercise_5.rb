c92d47d52be4119b04291a10f23afdbcea00ee49
keano voelkl <keanov@gmail.com>
2017-08-01 17:00:58 +0000

    ex5 done

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -10,9 +10,14 @@ puts "----------"
 # Your code goes here ...
 
 @sum_annual_rev = Store.sum("annual_revenue")
+@avg_annual_rev_per_store = @sum_annual_rev / Store.count
 
-ptus @sum_annual_rev
+puts 'this is the total annual rev'
+puts @sum_annual_rev
 
+puts 'this is avg rev per store'
+puts @avg_annual_rev_per_store
 
+@num_stores_over_1_million = Store.where("annual_revenue > ?", 1000000).count
 
-
+puts @num_stores_over_1_million
