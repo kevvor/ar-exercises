3b07319d7b6ec6b766a7b1bb4262b65f0ee1eda9
keano voelkl <keanov@gmail.com>
2017-08-01 16:16:09 +0000

    ex1 finished

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,40 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+class Store < ActiveRecord::Base
+
+  validates :name, presence: true
+  validates :annual_revenue, presence: true
+
+end
+
+burnaby = Store.create do |b|
+  b.name = "Burnaby"
+  b.annual_revenue = 300000
+  b.mens_apparel = true
+  b.womens_apparel = true
+end
+
+richmond = Store.create do |r|
+  r.name = "Richmond"
+  r.annual_revenue = 1260000
+  r.mens_apparel = false
+  r.womens_apparel = true
+end
+
+gastown = Store.create do |g|
+  g.name = "Gastown"
+  g.annual_revenue = 190000
+  g.mens_apparel = true
+  g.womens_apparel = false
+end
+
+Store.count
+
+
+
+
+
+
+
